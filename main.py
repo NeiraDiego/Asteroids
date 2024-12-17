5a37fd3efded3cd06fc60b9a573847a4b5093d79
NeiraDiego <111712185+NeiraDiego@users.noreply.github.com>
2024-12-16 22:04:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,47 @@
 import pygame
 from constants import *
-from circleshape import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_fied = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    
 
-    screen.fill("black")
-    pygame.display.flip()
-    dt = clock.tick(60) / 1000
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limita los FPS a 60
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
